aea8440cefda7e785c9ea25a6db67a655efd047c
Claudio Baeza <cl.baeza@gmail.com>
2019-03-15 17:49:37 -0300

    Add ADT A04

--- Sources/mllp/Messages.swift
+++ Sources/mllp/Messages.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 public struct Messages {
+    
     public static func A01(
         admitInpatient          VersionID: String?,
         sendingRisName              MSH_3: String?,
@@ -30,6 +31,52 @@ public struct Messages {
             ReceivingApplication:MSH_5,
             ReceivingFacility   :MSH_6,
             MessageType         :"ADT^A01^ADT_A01",
+            MessageControlID    :MSH_10,
+            VersionID           :VersionID,
+            CountryCode         :MSH_17,
+            CharacterSet        :stringEncoding,
+            PrincipalLanguage   :MSH_19
+        )
+        
+        guard let _MSH = MSH else { return nil }
+        
+        let PID: String? = Segments.PID(
+            PatientIdentifierList   :PID_3,
+            PatientName             :PID_5,
+            MotherMaidenName        :nil,
+            PatientBirthDate        :PID_7,
+            PatientAdministrativeSex:PID_8
+        )
+        
+        guard let _PID = PID else { return nil }
+        
+        return String(format: "%@\r%@\r", _MSH, _PID)
+    }
+    
+    //----------------------------------------------------------------------
+    
+    public static func A04(
+        registerPatient         VersionID: String?,
+        sendingRisName              MSH_3: String?,
+        sendingRisIP                MSH_4: String?,
+        receivingCustodianTitle     MSH_5: String?,
+        receivingPacsaet            MSH_6: String?,
+        MessageControlId           MSH_10: String?,
+        CountryCode                MSH_17: String?,
+        stringEncoding: CFStringEncoding?,
+        PrincipalLanguage          MSH_19: String?,
+        PatientIdentifierList       PID_3: String,
+        PatientName                 PID_5: String,
+        PatientBirthDate            PID_7: String?,
+        PatientAdministrativeSex    PID_8: String?
+        ) -> String? {
+        
+        let MSH: String? = Segments.MSH(
+            SendingApplication  :MSH_3,
+            SendingFacility     :MSH_4,
+            ReceivingApplication:MSH_5,
+            ReceivingFacility   :MSH_6,
+            MessageType         :"ADT^A04^ADT_A01",
             MessageControlID    :MSH_10,
             VersionID           :VersionID,
             CountryCode         :MSH_17,
