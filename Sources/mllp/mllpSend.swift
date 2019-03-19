5e3590dfa038c3e2c3b070dd1aa5f095473b19d2
Claudio Baeza <cl.baeza@gmail.com>
2019-03-19 08:53:13 -0300

    mllpSend

---
+++ Sources/mllp/mllpSend.swift
@@ -0,0 +1,83 @@
+//
+//  mllpSend.swift
+//  mllp
+//
+//  Created by cbaeza on 3/18/19.
+//
+
+import Foundation
+
+public struct mllpSend {
+    
+
+    public static func send(
+        to ipString: String,
+        port portString: String,
+        message: String,
+        stringEncoding: CFStringEncoding,
+        payload: inout String
+        ) -> Bool
+    {
+        var SB: UInt8=0x0b
+        var EB: UInt8=0x1c
+        var CR: UInt8=0x0d
+        //create stream structures
+        var inputStream: InputStream?
+        var outputStream: OutputStream?
+        Stream.getStreamsToHost(withName: ipString, port: Int(portString)! , inputStream: &inputStream, outputStream: &outputStream)
+        if (outputStream==nil)
+        {
+            payload.append(contentsOf: String(format:"can not create output stream to %@:%@",ipString,portString))
+            return false;
+        }
+        if (inputStream==nil)
+        {
+            payload.append(contentsOf: String(format:"can not create input stream to %@:%@",ipString,portString))
+            return false;
+        }
+        var encode: String.Encoding
+        switch (stringEncoding) {
+        case 1://ascii
+            encode=String.Encoding.ascii
+        case 4://utf-8
+            encode=String.Encoding.utf8
+        default:
+            encode=String.Encoding.isoLatin1
+        }
+        //send message
+        outputStream?.open()
+        var data = Data()
+        data.append(&SB, count: 1)
+        data.append(message.data(using: encode)!)
+        data.append(&EB, count: 1)
+        data.append(&CR, count: 1)
+        //Data in swift does not allow to obtain the bytes, it must be copied in UnsafeMutablePointer
+        let dataMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: data.count)
+        data.copyBytes(to: dataMutablePointer, count: data.count)
+        let dataPointer = UnsafePointer<UInt8>(dataMutablePointer)
+        outputStream?.write(dataPointer, maxLength: data.count)
+        //Get error
+        let outputStreamError = outputStream?.streamError
+        outputStream?.close()
+        outputStream = nil
+        if (outputStreamError != nil)
+        {
+            payload.append(contentsOf: String(format:">%@:%@\r\n%@",ipString,portString, outputStreamError.debugDescription))
+            return false;
+        }
+        //receive payload
+        inputStream?.open()
+        let readMutablePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: 1024)
+        inputStream?.read(readMutablePointer, maxLength: 1024)
+        let inputStreamError = inputStream?.streamError
+        inputStream?.close()
+        inputStream = nil
+        if (inputStreamError != nil)
+        {
+            payload.append(contentsOf: String(format:"<%@:%@\r\n%@",ipString,portString, inputStreamError.debugDescription))
+            return false;
+        }
+        payload.append(contentsOf: String(bytesNoCopy: readMutablePointer, length: 1024, encoding: String.Encoding.ascii, freeWhenDone: true)! )
+        return true
+    }
+}
--- Tests/mllpTests/mllpTests.swift
+++ Tests/mllpTests/mllpTests.swift
@@ -2,14 +2,21 @@ import XCTest
 @testable import mllp
 
 final class mllpTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(mllp().text, "Hello, World!")
+    func testHL7Send() {
+        let hl7 = #"""
+MSH|^~\&|||||20190311||ORM^O01|01517B57-9DD7-4152-A8D3-F6112F1E878D|P|2.3.1|||||cl|8859/1|es
+PID|||11111^^^2.16.858.1.152.68909||MURILLO>TORO^HERNANDO||19620226|M|
+PV1|||||||||||||||||||
+ORC|NW|IMATEC^20190311103302|IMATEC_LASERENA^20190311103302||SC||^^^20190311103302^^T|||||||||||127.0.0.1:52578
+OBR||||0401051^1^RADIOGRAFIA DE PELVIS, CADERA O COXOFEMORAL||||||||||||-^-^-^MEDICO TRATANTE .||OR19H664CR1|574003982|574003982||||CR||||||||IMATEC^CR^fblumel||IMATEC^CR^fblumel||||||||||0401051^1^RADIOGRAFIA DE PELVIS, CADERA O COXOFEMORAL
+ZDS|2.25.354145732298734539126994298128312842752
+"""#
+        var payload: String = ""
+        let result = mllpSend.send(to: "127.0.0.1", port: "50530", message: hl7, stringEncoding: 1, payload: &payload)
+        XCTAssertEqual(result, true)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testHL7Send", testHL7Send),
     ]
 }
